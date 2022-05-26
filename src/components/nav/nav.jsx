e79668a8459882fbe1247d5890d45eac693aaf36
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-05-26 18:11:25 +0100

    updating nav

---
+++ src/components/nav/nav.jsx
@@ -0,0 +1,27 @@
+import "./nav.css";
+
+function Navbar() {
+  return (
+    <nav class="navbar">
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects # 1</a>
+        </li>
+        <li>
+          <a href="#projects2">Projects # 2</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
