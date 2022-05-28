8742e32124b5c25af75639ac3676990ed9ab36e2
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-05-28 11:40:43 +0100

    updating imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import Home from "./components/home/home";
+import Nav from "./components/nav/nav";
 import About from "./components/about/about";
 import Contact from "./components/contact/contact";
 import Projects from "./components/projects/projects";
@@ -7,6 +9,7 @@ function WebContent() {
   return (
     <>
       <Home />
+      <Nav />
       <About />
       <Projects />
       <Contact />
