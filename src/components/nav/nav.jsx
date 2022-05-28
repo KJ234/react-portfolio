8b65deeaa1a42bc6c99ec3e1fe2e34fa706b0c44
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-05-28 12:38:07 +0100

    adding nav section with css

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,26 +1,29 @@
 import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { TiGroupOutline } from "react-icons/ti";
+import { BsArrowDownCircle } from "react-icons/bs";
 
 function Navbar() {
   return (
-    <nav class="navbar">
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#projects">Projects # 1</a>
-        </li>
-        <li>
-          <a href="#projects2">Projects # 2</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <div className="navigation">
+      <a href="#home">
+        <AiOutlineHome className="icon active-nav" />
+      </a>
+
+      <a href="#about">
+        <AiOutlineUser className="icon" />
+      </a>
+      <a href="#projects">
+        <TiGroupOutline className="icon" />
+      </a>
+      <a href="#projects2">
+        <TiGroupOutline className="icon" />
+      </a>
+      <a href="#contact">
+        <BsArrowDownCircle className="icon" />
+      </a>
+    </div>
   );
 }
 
