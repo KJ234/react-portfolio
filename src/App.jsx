0920598fe5ec00c2d237b290191ff09b9112a326
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-05-28 11:40:26 +0100

    updating imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import React from "react";
-
 import Home from "./components/home/home";
-import Nav from "./components/nav/nav";
 import About from "./components/about/about";
-import Skills from "./components/skills/skills";
-import Projects from "./components/projects/projects";
 import Contact from "./components/contact/contact";
+import Projects from "./components/projects/projects";
 
-function webContent() {
+function WebContent() {
   return (
     <>
       <Home />
-      <Nav />
       <About />
-      <Skills />
       <Projects />
       <Contact />
     </>
   );
 }
-
-export default webContent;
+export default WebContent;
